0d09cda4a84ad13fc300d5f6f93c086fb88a3851
Brandon <brandon.lau86@gmail.com>
2019-11-03 18:21:19 -0500

    Create middleware folder

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const path = require("path");
 const members = require("./Members.js")
+const logger = require("./middleware/logger.js")
 const app = express();
+
+
 const PORT = process.env.PORT || 5000;
 
-//INIT middleware
-const logger = (req,res,next) => {
-  console.log("Hello");
-  next();
-}
-app.use(logger)
-
+//INIT middleware 
+app.use(logger);
 //GET "/api/members returns JSON"
 app.get("/api/members", (req,res) => 
 {
